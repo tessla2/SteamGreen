d5e6d8180fdfb584bd9090ee9e51051f23159188
tessla <tessiesants3@gmail.com>
2024-04-19 18:04:40 +0100

    Add project files.

---
+++ SteamGrenn.Domain/Entities/Villa.cs
@@ -0,0 +1,16 @@
+﻿namespace WhiteLagoon.Domain.Entitis
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+
+}
